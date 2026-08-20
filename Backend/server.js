6661abd327df1bacdca688efff689b6489fed666
Divyansh <Divyanshgautam7115@gmail.com>
2026-08-20 20:17:53 +0530

    login page created on backend

--- Backend/server.js
+++ Backend/server.js
@@ -4,11 +4,19 @@ const cors = require("cors");
 
 require("dotenv").config();
 
+const authenticationRoutes = require("./routes/auth");
+
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authenticationRoutes);
+
+
+
+// MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
